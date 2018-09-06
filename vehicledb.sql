890d6f30cb349af27b878255e4e7a6e707a68100
Anvesh1201 <43021088+Anvesh1201@users.noreply.github.com>
2018-09-06 11:14:26 -0500

    Add files via upload

---
+++ vehicledb.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Community Edition- MySQL GUI v6.07
+Host - 5.0.67-community-nt : Database - vehicledb
+*********************************************************************
+Server version : 5.0.67-community-nt
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+create database if not exists `vehicledb`;
+
+USE `vehicledb`;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `vehicleinfo` */
+
+DROP TABLE IF EXISTS `vehicleinfo`;
+
+CREATE TABLE `vehicleinfo` (
+  `vehicleId` int(11) default NULL,
+  `vehicleOwnerName` varchar(255) default NULL,
+  `vehicleName` varchar(255) default NULL,
+  `vehiclecost` varchar(255) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `vehicleinfo` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
